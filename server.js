9d238f5212fbef8ea86356a7d1bf8e5aa1a77760
newsha <nioosha.h@gmail.com>
2020-12-29 18:52:11 +0330

    did a bit of preperation for adding CREATE route

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const port = 8000;
+
+require("./app/routes")(app, {});
+
 app.listen(port, () => {
   console.log("server is listening on " + port);
 });
