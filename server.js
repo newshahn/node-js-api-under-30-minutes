239d1f8a4232c8f371ce3ddd6581de12abd7f609
newsha <nioosha.h@gmail.com>
2021-01-02 16:28:04 +0330

    added a gitignore file and connected server to mongo db

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
+const db = require("./config/db");
 
 const app = express();
 
 const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(db.url, (err, database) => {
+  const database = database.db("note-api");
+  if (err) return console.log(err);
+});
 
 require("./app/routes")(app, {});
 
