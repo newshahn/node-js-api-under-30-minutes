6625a547cf3b098844d08fd5d049cbd1adf37077
newsha <nioosha.h@gmail.com>
2021-01-02 17:52:38 +0330

    tried to make a read route. tested it with postman but I don't get any result back. don't know why.

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,14 +1,47 @@
+const db = require("../../config/db");
+var ObjectID = require("mongodb").ObjectID;
+
+//CREATE
+// module.exports = function (app, db) {
+//   const collection = app.post("/notes", (req, res) => {
+//     const note = { text: req.body.body, title: req.body.title };
+//     db.collection("notes").insert(note, (err, result) => {
+//       if (err) {
+//         res.send("An error has occurred.");
+//       } else {
+//         res.send(result.ops[0]);
+//       }
+//     });
+//   });
+// };
+
+//READ
+
 module.exports = function (app, db) {
-  const collection = app.post("/notes", (req, res) => {
-    const note = { text: req.body.body, title: req.body.title };
-    db.collection("notes").insert(note, (err, result) => {
+  app.get("/notes/:id", (req, res) => {
+    const id = req.params.id;
+    const details = { " _id": new ObjectID(id) };
+    db.collection("notes").findOne(details, (err, item) => {
       if (err) {
-        res.send("An error has occurred.");
+        return res.send({ error: "an error has ocurred." });
       } else {
-        res.send(result.ops[0]);
+        return res.send(item);
       }
     });
-    // console.log(req.body);
-    // res.send("Hello");
+  });
+
+  app.post("/notes", (req, res) => {
+    const note = {
+      text: req.body.body,
+      title: req.body.title,
+    };
+
+    db.collection("notes").insert(note, (err, result) => {
+      if (err) {
+        return res.send({ error: "an error has occurred." });
+      } else {
+        return res.send(result.ops[0]);
+      }
+    });
   });
 };
