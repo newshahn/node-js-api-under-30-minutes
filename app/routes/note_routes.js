95b75a1af59a1dbcce5782f4c165a03dbe748c8a
Newsha Nik <nioosha.h@gmail.com>
2021-01-02 21:22:51 +0330

    added the delete route

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -2,18 +2,18 @@ const db = require("../../config/db");
 var ObjectID = require("mongodb").ObjectID;
 
 //CREATE
-// module.exports = function (app, db) {
-//   const collection = app.post("/notes", (req, res) => {
-//     const note = { text: req.body.body, title: req.body.title };
-//     db.collection("notes").insert(note, (err, result) => {
-//       if (err) {
-//         res.send("An error has occurred.");
-//       } else {
-//         res.send(result.ops[0]);
-//       }
-//     });
-//   });
-// };
+module.exports = function (app, db) {
+  const collection = app.post("/notes", (req, res) => {
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection("notes").insert(note, (err, result) => {
+      if (err) {
+        res.send("An error has occurred.");
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+};
 
 //READ
 
@@ -45,3 +45,17 @@ module.exports = function (app, db) {
     });
   });
 };
+
+//DELETE
+
+app.delete("/notes/:id", (req, res) => {
+  const id = req.params.id;
+  const details = { _id: new ObjectID(id) };
+  db.collection("notes").remove(details, (err, item) => {
+    if (err) {
+      res.send({ error: "An error has occurred" });
+    } else {
+      res.send("Note " + id + " deleted!");
+    }
+  });
+});
