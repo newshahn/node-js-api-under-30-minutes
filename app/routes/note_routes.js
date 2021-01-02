fad04c4ef7b256f5de7be470356669ed433ada61
Newsha Nik <nioosha.h@gmail.com>
2021-01-02 21:47:17 +0330

    no matter what I do, I cannot get this program to work again

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -59,3 +59,18 @@ app.delete("/notes/:id", (req, res) => {
     }
   });
 });
+
+//UPDATE
+
+app.put("/notes/:id", (req, res) => {
+  const id = req.params.id;
+  const details = { _id: new ObjectID(id) };
+  const note = { text: req.body.body, title: req.body.title };
+  db.collection("notes").update(details, note, (err, result) => {
+    if (err) {
+      res.send({ error: "An error has occurred" });
+    } else {
+      res.send(note);
+    }
+  });
+});
